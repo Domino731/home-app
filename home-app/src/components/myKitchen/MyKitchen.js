3b181849421344bb790c5485c547bf648b1e7724
Domino731 <d.orzechowski18@gmail.com>
2021-06-19 17:00:29 +0200

    automatic rendering by firebase

--- home-app/src/components/myKitchen/MyKitchen.js
+++ home-app/src/components/myKitchen/MyKitchen.js
@@ -1,20 +1,49 @@
 import MyKitchenBar from "./MyKitchenBar";
 import MyKitchenCategory from "./MyKitchenCategory";
 
-export const MyKitchen = () => {
+import {useEffect, useState} from "react";
+import {db} from "../../fireBase/fireBase";
+import {Loading} from "../loading/Loading";
 
+export const MyKitchen = () => {
+    const [renderingArray, setRenderingArray] = useState([])
+    useEffect(() => {
+        db.collection("productsRendering").get().then((querySnapshot) => {
+            const data = querySnapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+            setRenderingArray(data.sort((a,b) => a.number - b.number ))
+        });
+    }, [])
+
+
+
+
+
+    if(renderingArray.length === undefined){
+        return null
+    }
+    if(renderingArray.length === 0){
+        return <Loading/>
+    }
     return (
         <section className="container">
             <MyKitchenBar/>
-            <MyKitchenCategory title="Warzywa" productType={"vegetables"}/>
-            <MyKitchenCategory title="Mięso" productType={"meat"}/>
-            <MyKitchenCategory title="Nabiał" productType={"dairy"}/>
-            <MyKitchenCategory title="Cukry" productType={"sugar"}/>
-            <MyKitchenCategory title="Owoce" productType={"fruit"}/>
-            <MyKitchenCategory title="Mrożonki" productType={"frozenFood"}/>
-            <MyKitchenCategory title="Napoje" productType={"drinks"}/>
-            <MyKitchenCategory title="Inne" productType={"others"}/>
+            {
+                renderingArray.map(el => <MyKitchenCategory title={el.title} productType={el.productType} key={el.id}/>)
+            }
+
         </section>
     )
 }
 
+
+// {/*<MyKitchenCategory title="Warzywa" productType={"vegetables"}/>*/}
+// {/*<MyKitchenCategory title="Mięso" productType={"meat"}/>*/}
+// {/*<MyKitchenCategory title="Nabiał" productType={"dairy"}/>*/}
+// {/*<MyKitchenCategory title="Cukry" productType={"sugar"}/>*/}
+// {/*<MyKitchenCategory title="Owoce" productType={"fruit"}/>*/}
+// {/*<MyKitchenCategory title="Mrożonki" productType={"frozenFood"}/>*/}
+// {/*<MyKitchenCategory title="Napoje" productType={"drinks"}/>*/}
+// {/*<MyKitchenCategory title="Inne" productType={"others"}/>*/}
--- home-app/src/components/myRecipes/MyRecipes.js
+++ home-app/src/components/myRecipes/MyRecipes.js
@@ -1,19 +1,41 @@
 import {MyRecipesBar} from "./MyRecipesBar";
 import {MyRecipeRedirect} from "./MyRecipeRedirect";
+import {useEffect, useState} from "react";
+import {db} from "../../fireBase/fireBase";
+import {Loading} from "../loading/Loading";
 
 export const MyRecipes = () => {
+    const [renderingArray, setRenderingArray] = useState([])
+    useEffect(() => {
+        db.collection("productsRendering").get().then((querySnapshot) => {
+            const data = querySnapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+            setRenderingArray(data.sort((a,b) => a.number - b.number ))
+        });
+    }, [])
+    if(renderingArray.length === undefined){
+        return null
+    }
+    if(renderingArray.length === 0){
+        return <Loading/>
+    }
     return (
         <section className="container">
             <MyRecipesBar/>
             <div className="recipesType">
-                <MyRecipeRedirect title={"Ciasta"} path="cakes"/>
-                <MyRecipeRedirect title={"Desery"} path="desserts"/>
-                <MyRecipeRedirect title={"Obiady"} path="dinners"/>
-                <MyRecipeRedirect title={"Sałatki"} path="salads"/>
-                <MyRecipeRedirect title={"Zupy"} path="soup"/>
-                <MyRecipeRedirect title={"Na szybko"} path="fasts"/>
-                <MyRecipeRedirect title={"Specjalne"} path="specials"/>
+                {
+                    renderingArray.map(el => <MyRecipeRedirect title={el.title} path={el.path}/> )
+                }
             </div>
         </section>
     )
 }
+// {/*<MyRecipeRedirect title={"Ciasta"} path="cakes"/>*/}
+// {/*<MyRecipeRedirect title={"Desery"} path="desserts"/>*/}
+// {/*<MyRecipeRedirect title={"Obiady"} path="dinners"/>*/}
+// {/*<MyRecipeRedirect title={"Sałatki"} path="salads"/>*/}
+// {/*<MyRecipeRedirect title={"Zupy"} path="soup"/>*/}
+// {/*<MyRecipeRedirect title={"Na szybko"} path="fasts"/>*/}
+// {/*<MyRecipeRedirect title={"Specjalne"} path="specials"/>*/}
