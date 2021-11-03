05cb557df097ca9f512ad966cf3505ede29e2c90
Domino731 <d.orzechowski18@gmail.com>
2021-11-03 20:54:17 +0100

    general - kitchen products

--- src/components/myKitchen/MyKitchenProduct.js
+++ src/components/myKitchen/MyKitchenProduct.js
@@ -1,32 +1,29 @@
-//component displaying a single product, is rendered in MyKitchenCategory component
+import { useEffect, useState } from "react";
+import { auth, db } from "../../fireBase/fireBase";
+import { deleteDataFirestore } from "../../fireBase/deleteDataFirestore";
+import { updateDataFirestore } from "../../fireBase/updateDataFirestore";
+import { getUnit } from "../../functions/getUnit";
 
-import {useEffect, useState} from "react";
-import {connect} from "react-redux";
-import {db} from "../../fireBase/fireBase";
-import {deleteDataFirestore} from "../../fireBase/deleteDataFirestore";
-import {updateDataFirestore} from "../../fireBase/updateDataFirestore";
-import {getUnit} from "../../functions/getUnit";
+/**
+ * Component which is rendering single product container with ability to display form by which user can edit this product 
+ * @param prod - object with data about single product
+ * @param id - id of product, needed to update or delete product
+ */
+const MyKitchenProduct = ({ prod, id, }) => {
 
-
-// props //
-// prod --> actual product and information about him
-// id --> product id, used in update or delete product functions
-// username --> current username, used in update or delete product functions
-const MyKitchenProduct = ({prod, id, username}) => {
-
-    //flag that allows you to display a form to edit the recipe
+    // flag that allows user to toggle form by which user can edit his product (change amount or delete)
     const [flag, setFlag] = useState(true)
 
-    //state which adds a class responsible for animations in the product management
+    //s tate which adds a class responsible for animations in the product management
     const [successfulUpdate, setSuccessfulUpdate] = useState(false)
 
-    //state holding new amount of product
+    // state with product amount
     const [newAmount, setNewAmount] = useState(prod.amount)
 
-    //state containing the icon that is downloaded when the component is mounted
+    // state containing the icon that is downloaded from  when the component is mounted 
     const [icon, setIcon] = useState("")
 
-    //getting the icon
+    // get the icon (<i> tag)of product type from 'productsRendering' collection in firestore
     useEffect(() => {
         db.collection("productsRendering")
             .where("productType", "==", `${prod.type}`)
@@ -35,31 +32,21 @@ const MyKitchenProduct = ({prod, id, username}) => {
                 const data = querySnapshot.docs.map(doc => (
                     doc.data().icon
                 ));
-                setIcon(data[0])
+                setIcon(data[0]);
             });
-    }, [])
+    }, []);
 
+    /** change flag state -> toggle product management form  */
+    const handleChangeFlag = () => flag ? setFlag(false) : setFlag(true);
 
-    //function which shows or hide product management
-    const handleChangeFlag = () => {
-        if (flag) {
-            setFlag(false)
-        } else {
-            setFlag(true)
-        }
-    }
+    /** function which is changing the amount state */
+    const handleChangeAmount = (e) => setNewAmount(e.target.value);
 
-    //function which changes the amount state
-    const handleChangeAmount = (e) => setNewAmount(e.target.value)
+    /** function which increase the amount state ( +1 ) */
+    const handleIncrease = () => setNewAmount(prev => prev + 1);
 
-    //function which increase the amount state
-    const handleIncrease = () => {
-        setNewAmount(prev => prev + 1)
-    }
-
-    //function which decrease the amount state
+    /** function which decrease the amount state ( -1 ) */
     const handleDecrease = () => {
-        console.log(true)
         if (newAmount === 0) {
             setNewAmount(0)
         } else {
@@ -68,70 +55,67 @@ const MyKitchenProduct = ({prod, id, username}) => {
     }
 
     //functions which hide  product management
+    /** function that changes successfulUpdate state (animation is displayed), and hides management form after delay (650) */
     const handleClose = () => {
-        setSuccessfulUpdate(true)
-        setTimeout(() => {
-            setFlag(true)
-            setSuccessfulUpdate(false)
-        }, 650)
-
+        setSuccessfulUpdate(true);
+        return setTimeout(() => {
+            setFlag(true);
+            setSuccessfulUpdate(false);
+        }, 650);
     }
 
+    return <>
+        <section className="kitchenProduct" onClick={handleChangeFlag}>
+            {/*icon of product type*/}
+            <i className={icon} />
 
-    return (
-        <>
-            <section className="kitchenProduct" onClick={handleChangeFlag}>
+            {/* product name */}
+            <strong className="kitchenProduct__name">{prod.name}</strong>
 
-                {/*icon from firestore*/}
-                <i className={icon}/>
-                <strong className="kitchenProduct__name">{prod.name}</strong>
-                <div className="kitchenProduct_amount">
+            {/* product amount */}
+            <div className="kitchenProduct_amount">
+                {/*If the amount is 0, display a message that the product is missing*/}
+                {prod.amount > 0 && <>
+                    <strong>
+                        {prod.amount ? prod.amount : `Brak`}
+                    </strong>
 
-                    {/*If the amount is 0, display a message that the quantity is missing*/}
-                    {prod.amount > 0 && <>
-                        <strong>
-                            {prod.amount !== "" ? prod.amount : "Brak"}
-                        </strong>
+                    <strong>{getUnit(prod.unit, prod.amount)}</strong>
+                </>}
+                {prod.amount === 0 && <strong>Brak</strong>}
+            </div>
 
-                        <strong>{getUnit(prod.unit, prod.amount)}</strong>
-                    </>}
-                    {prod.amount === 0 && <strong>Brak</strong>}
+        </section>
 
+        {/*product management is displayed when you click on a product*/}
+        {flag === false &&
 
-                </div>
-            </section>
-
-            {/*product management is displayed when you click on a product*/}
-            {flag === false &&
+             
             <section className={`kitchenProductManagement ${successfulUpdate && "successfulProductUpdate"}`}>
 
-                <h3 className="management__title"><i className="fas fa-times-circle" onClick={handleChangeFlag}/> Edytuj
+                <h3 className="management__title"><i className="fas fa-times-circle" onClick={handleChangeFlag} /> Edytuj
                 </h3>
                 <div className="management__amount">
 
                     {/*changing the amount*/}
-                    <button onClick={handleIncrease}><i className="fas fa-plus-circle"/></button>
-                    <input type="number" placeholder="nowa ilość" value={newAmount} onChange={handleChangeAmount}/>
-                    <button onClick={handleDecrease}><i className="fas fa-minus-circle"/></button>
+                    <button onClick={handleIncrease}><i className="fas fa-plus-circle" /></button>
+                    <input type="number" placeholder="nowa ilość" value={newAmount} onChange={handleChangeAmount} />
+                    <button onClick={handleDecrease}><i className="fas fa-minus-circle" /></button>
 
                 </div>
                 <div className="management__actions">
 
                     {/*deleting product*/}
-                    <button onClick={() => deleteDataFirestore(id, username, "products")}>Usuń produkt</button>
+                    <button onClick={() => deleteDataFirestore(id, auth().currentUser.uid, "products")}>Usuń produkt</button>
 
                     {/*updating product*/}
                     <button
-                        onClick={() => updateDataFirestore(id, username, "products", {amount: newAmount}, handleClose)}>Zapisz
+                        onClick={() => updateDataFirestore(id, auth().currentUser.uid, "products", { amount: newAmount }, handleClose)}>Zapisz
                         produkt
                     </button>
                 </div>
             </section>}
-        </>
-    )
+    </>
 }
-//the username so that the function knows for which user to send the updated product
-const mapStateToProps = state => ({
-    username: state.currentUser.displayName
-})
-export default connect(mapStateToProps)(MyKitchenProduct)
+
+export default MyKitchenProduct;
