b1f845a3b30d9c7fe06c8bf1796e81442406e852
Domino731 <d.orzechowski18@gmail.com>
2021-11-05 16:04:40 +0100

    styles - auth - styles for forms

--- src/components/userForm/Login.js
+++ src/components/userForm/Login.js
@@ -1,11 +1,16 @@
 //component responsible for user login, is used in UserForm component
-import {useHistory} from "react-router-dom";
-import {useState} from "react";
-import {auth} from "../../fireBase/fireBase";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../../fireBase/fireBase";
 
 // props //
 // changeForm --> changing form to register
 export const Login = () => {
+
+    // state with date needed to loggin user
+    const [data, setData] = useState({ email: "", password: "" });
+
+    const [invalid, setInvalid] = useState({ email: false, password: false});
 
     // //state with user data
     // const [data, setData] = useState({email: "", password: ""})
@@ -51,9 +56,44 @@ export const Login = () => {
 
     //history
     let history = useHistory();
+
+    const handleChangeData = (e) => {
+        const {name, value} = e.target;
+        return setData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
     return <main className="auth">
-        asdasd
-        </main>
-    
+        <h1 className="auth__title">Zaloguj</h1>
+        <form className="auth__form">
+            <label className={`auth__label ${invalid.email ? 'auth__label--inCorrect' : 'auth__label--correct'}`}>
+                Podaj e-mail
+                <i className="fas fa-at" />
+                <input 
+                name='email' 
+                value={data.email} 
+                className="auth__input" 
+                onChange={handleChangeData}
+                 required/>
+                <span>E-mail</span>
+            </label>
+            <label className={`auth__label ${invalid.password ? 'auth__label--inCorrect' : 'auth__label--correct'}`}>
+                Podaj hasło
+                <i className="fas fa-lock"/>
+                <input name='password' 
+                value={data.password} 
+                className="auth__input" 
+                onChange={handleChangeData}
+                 required/>
+                <span>Hasło</span>
+            </label>
+            <div className="auth__error">Nieprawidłowy e-mail</div>
+            <div className="auth__error">Nieprawidłowe hasło</div>
+            <button className="auth__btn">Zaloguj się</button>
+        </form>
+    </main>
+
 }
 
